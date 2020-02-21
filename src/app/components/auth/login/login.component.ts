13be5c46d31ff7fe1621e93c7ccc151732f2367a
unknown <ps287649@gmail.com>
2020-02-21 15:43:08 +0530

    login.signup,otp verification,otp resend

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api/api.service';
+import { LOGIN } from 'src/config';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiService,private router:Router) { }
+d:any;
+  login(value)
+  {
+      console.log(value);
+      this.api.Post(LOGIN,value).then(data=>{
+      this.d=data['data'][0];
+         console.log(this.d);
+         if(data['data'])
+         {
+            this.router.navigate(['/home']);
+            localStorage.setItem('data',JSON.stringify(this.d));   
+         }
+
+        });
+
+  }
 
   ngOnInit() {
   }
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
 export const apiUrl = 'http://newapp.savyajewels.net/api/';
-
+// authentication
 export const LOGIN = 'user/login';
+export const REGISTER = 'user/register';
+export const otpverified= 'user/register/otpverified';
+export const otpresend= 'user/register/otpresend';
+
