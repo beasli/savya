1a83b888d3fe5a992ee4d0ade8bfcb851c712b86
Lokesh2096 <lokeshkumar2096@gmail.com>
2020-03-02 14:47:11 +0530

    authguard done

--- src/app/components/auth/change-profile/change-profile.component.ts
+++ src/app/components/auth/change-profile/change-profile.component.ts
@@ -8,50 +8,44 @@ import { PROFILEUPDATE } from 'src/config';
   styleUrls: ['./change-profile.component.css']
 })
 export class ChangeProfileComponent implements OnInit {
-  loading:boolean;
-  sign:boolean=true;
-  data:any;
-  alert:boolean;
-  message:any;
-  type:any;
-  d:any;
+  loading: boolean;
+  sign: boolean = true;
+  data: any;
+  alert: boolean;
+  message: any;
+  type: any;
+  d: any;
 
-  constructor(private api:ApiService) {
-    this.data=JSON.parse(localStorage.getItem('data'));
+  constructor(private api: ApiService) {
+    this.data = JSON.parse(localStorage.getItem('data'));
     console.log(this.data);
    }
 
-   
-   update(value){
-    
-    this.loading=true;
-    this.sign=false;
+
+   update(value) {
+
+    this.loading = true;
+    this.sign = false;
     // console.log(value);
-     
-    this.api.Post(PROFILEUPDATE,value).then(data=>{
-      
+    this.api.Post(PROFILEUPDATE, value).then(data => {
+
               console.log(data);
-              this.alert=true;
-              this.message="Successful Updated "
-              this.type="success";
-              this.d=data['data'][0];
+              this.alert = true;
+              this.message = 'Successful Updated';
+              this.type = 'success';
+              this.d = data['data'][0];
               console.log(this.d);
-              localStorage.setItem('data',JSON.stringify(this.d));   
-              //this.router.navigate(['/registerOtp']);
-      }).catch(d=>{
-              
-              this.type="danger";
-              this.loading=false;
-              this.sign=true;
-              this.alert=true;
-              this.message="Enter your all detail";
+              localStorage.setItem('data', JSON.stringify(this.d));
+      }).catch(d => {
+
+              this.type = 'danger';
+              this.loading = false;
+              this.sign = true;
+              this.alert = true;
+              this.message = 'Enter your all detail';
               console.log(d);
-
-
       });
-  
 }
-
 
   ngOnInit(): void {
 }
