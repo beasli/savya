11ee40686f0a58da91c6f835cd3e2dd6e6734e7a
unknown <ps287649@gmail.com>
2020-03-18 10:53:00 +0530

    upto filter with minor issue

--- src/config.ts
+++ src/config.ts
@@ -51,4 +51,6 @@ export const CATEGORY = 'user/category/all';
 export const SUBCATEGORY = 'user/subcategory/single';
 export const SUBCATEGORYTYPE = 'user/subsubcategory/single';
 export const PRODUCTLIST = 'product/list';
-
+export const PRODUCTFILTERMENU = 'search/filter/menu';
+export const PRODUCTFILTER = 'search/filter';
+export const ORDERBY = 'product/list?subsubcategory_id=';
