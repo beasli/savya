7f52138ad50204bc1ad31a14213ad46a10e363a7
unknown <ps287649@gmail.com>
2020-05-15 10:51:19 +0530

    profile pic

--- src/app/components/auth/kyc-pending/kyc-pending.component.ts
+++ src/app/components/auth/kyc-pending/kyc-pending.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/api/api.service';
-
+import { Router } from '@angular/router';
+import { PROFILEUPDATE,IMAGE, PROFILEVIEW } from 'src/config';
+const formData: FormData = new FormData();
+declare var $: any;
 @Component({
   selector: 'app-kyc-pending',
   templateUrl: './kyc-pending.component.html',
@@ -8,12 +11,97 @@ import { ApiService } from 'src/app/api/api.service';
 })
 export class KycPendingComponent implements OnInit {
 user:any;
-  constructor(private api:ApiService) {
-    this.user=this.api.getUserInfo();
+name:any;
+loader:boolean;
+page:boolean=true;
+  constructor(private api:ApiService,private router:Router) {
+    // this.user=this.api.getUserInfo();
+    // this.name=this.user.name;
     console.log(this.user);
+    this.api.Post(PROFILEVIEW, {}).then(data=>{
+      this.page=true;
+      this.loader=false;
+      console.log(data);
+      this.user=data['user'];
+      this.name=this.user.name;
+      //this.router.navigate(['/registerOtp']);
+    }).catch(d=>{
+      if(d.error.message == 'Unauthenticated.' && d.status == 401){
+        this.api.onFail('Your session is expired please login again');
+        this.api.setGoto();
+        this.api.setlogin(0);
+        this.api.logout();
+        setTimeout(() => {
+        this.router.navigate(['/login']);
+        },1000);
+      } else{
+        console.log(d);
+      }
+});
    }
+   changename(e)
+   {
+     this.name=e;
+     
+   }
+   changeImage(event)
+   {
+     let files:FileList=event.target.files;
+     formData.append('image', files.item(0), files.item(0).name);
+     if(files.length > 0)
+     {
+       let file: File = files[0];
+       var img = document.querySelector("#preview img");
+       var reader = new FileReader();
+                 reader.onload = function(e) {
+                   $('#image_preview').attr('src', e.target.result);
+                   console.log(e.target.result)
+                 }
+                reader.readAsDataURL(file); 
+     }
+   }
+   modal()
+  {
+    document.getElementById("image").click();
+  }
 
+   update(){
+    this.loader=true;
+    this.page=false;
+    formData.append('name',this.name);
+    if(this.name.length>0){
+      this.api.Post(PROFILEUPDATE, formData).then(data => {
+        this.page=true;
+        this.loader=false;
+          console.log(data);
+          this.api.setUserInfo(data['data']);
+          this.api.onSuccess("Profile successfully updated");
+        }).catch(d=>{
+          if(d.error.message == 'Unauthenticated.' && d.status == 401){
+            this.api.onFail('Your session is expired please login again');
+            this.api.setGoto();
+            this.api.setlogin(0);
+            this.api.logout();
+            setTimeout(() => {
+            this.router.navigate(['/login']);
+            },1000);
+          } else{
+            this.page=true;
+            this.loader=false;
+            this.api.onFail("Please try again");
+          }
+        });
+    }
+    else if(this.name.length==0){
+      this.page=true;
+        this.loader=false;
+      this.api.onFail("Name field can't be empty");
+    }
+    
+}
   ngOnInit(): void {
+    this.loader=true;
+    this.page=false;
   }
 
 }
